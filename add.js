fb54d6d322121727f539cb3ffe00d3b421818dae
Hakon einarsson <hakoneinarsson@Hakons-MacBook-Pro.local>
2018-10-09 16:58:28 +0000

    The function now captures negative numbers from the input and throws an error message

--- add.js
+++ add.js
@@ -13,12 +13,24 @@ function add(numbers) {
        var sum = 0;
        var array = numbers.split(/[,\n]/);
        var arrayLength = array.length;
-
+       var negatives = [];
+ 
        for(var i = 0; i < arrayLength; i++)
        {
+        if(parseInt(array[i]) < 0)
+        {
+            negatives[i] = array[i];
+        }
             sum = sum + parseInt(array[i]); // ParseInt : converts a string to an integer.
        }
-       return sum;
+       if(negatives.length > 0)
+       {
+        return "Negatives not allowed: " + negatives;
+       }
+       else
+       {
+        return sum;
+       }
     }
 } 
 module.exports = add;
--- add.test.js
+++ add.test.js
@@ -17,3 +17,7 @@ test("returns the number inputted if it was only a single number", () => {
 test("returns the number inputted if it was only a single number", () => { 
     expect(add("1\n2,3")).toBe(6);
 });
+
+test("Calling Add with a negative number will throw an exception “Negatives not allowed:“ listing all negative numbers that were in the list of numbers", () => { 
+    expect(add("-1, 2")).toBe("Negatives not allowed: -1");
+});
